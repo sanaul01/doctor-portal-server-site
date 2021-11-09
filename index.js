9d99d7f9807fecb75dbc51227d497c6715b486f0
sanaul01 <mdsanaulislam154@gmail.com>
2021-11-09 18:14:55 +0600

    serviceAccount added

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require('express');
 const { MongoClient} = require("mongodb");
 const app = express();
 const cors = require('cors');
+const admin = require("firebase-admin");
 require('dotenv').config();
 const port = process.env.PORT || 5000
+
+
+const serviceAccount = require('./doctors-portal-firebase-adminsdk.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 
 app.use(cors())
@@ -37,7 +45,7 @@ async function run(){
 
         app.get('/appointments', verifyToken, async (req, res)=>{
           const email= req.query.email;
-          const date = new Date(req.query.date).toLocaleDateString();
+          const date = req.query.date;
           const query = {email: email, date: date}
           const cursor = appointmentCollection.find(query);
           const appointments = await cursor.toArray()
